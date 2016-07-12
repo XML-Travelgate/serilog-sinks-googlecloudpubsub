721f743a5650647a3c4d798a07971d7a46501c40
Oscar Perez <oscar@xmltravelgate.com>
2016-07-12 10:57:25 +0200

    Tests

---
+++ test/Serilog.Sinks.GoogleCloudPubSub.Tests/BasicTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Xunit;
+
+namespace Serilog.Sinks.GoogleCloudPubSub.Tests
+{
+    public class BasicTests
+    {
+         [Fact]
+        public void TestOK()
+        {
+            Assert.True(true, $"Test true test");
+        }
+
+    }
+}
